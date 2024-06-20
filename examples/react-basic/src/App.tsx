0d19fbaf3abf490bf55213dca891dc328008a943
Qrj <renjie.qin@brain.im>
2024-06-20 14:18:12 +0800

    feat: fe-1 add react-basic example

--- examples/react-basic/src/App.tsx
+++ examples/react-basic/src/App.tsx
@@ -1,18 +1,3 @@
-## 简介
-
-slice-store 可以帮助您编写行为一致、在不同环境（客户端、服务器和本机）中运行且易于测试的 JavaScript 应用程序.
-
-## 安装
-
-```bash
-npm install @qlover/slice-store-react
-# or use yarn
-yarn add @qlover/slice-store-react
-```
-
-## 使用示例
-
-```tsx
 import { SliceStore } from '@qlover/slice-store';
 import { useSliceStore } from '@qlover/slice-store-react';
 import './App.css';
@@ -46,4 +31,3 @@ function App() {
 }
 
 export default App;
-```
